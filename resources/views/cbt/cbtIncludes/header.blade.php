6ee72b85ec82638f7e13f6a1714703128a7281d0
xenabyte <iconcept573@gmail.com>
2023-07-11 18:08:47 +0100

    update spinner

--- resources/views/cbt/cbtIncludes/header.blade.php
+++ resources/views/cbt/cbtIncludes/header.blade.php
@@ -20,6 +20,8 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.bootstrap.min.css" />
 
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.2.2/css/buttons.dataTables.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
 
 
 
